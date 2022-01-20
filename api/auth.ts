153ede23797d8765953b463564e42c3bf1546fd4
alli-eunbi <allieunbi@gmail.com>
2022-01-20 09:40:41 +0900

    추가 : "/me" 완성

--- api/auth.ts
+++ api/auth.ts
@@ -8,7 +8,6 @@ const auth = express();
 
 auth.post("/auth/signup", (req, res) => {
   const { name, email, password } = req.body;
-  console.log(name, email);
   if (!email || !password) {
     res.statusCode = 400;
     return res.send("회원가입 실패");
@@ -25,11 +24,8 @@ auth.post("/auth/signup", (req, res) => {
   }
   const buffer = fs.readFileSync("./users.json", { encoding: "utf8" });
   const { users }: { users: USER[] } = JSON.parse(buffer);
-  if (
-    users.some((users) => {
-      users.email === email;
-    })
-  ) {
+  if (users.some((_user) => _user.email === email)) {
+    res.statusCode = 400;
     return res.send("이미 회원이 있습니다.");
   }
   const newId = users[users.length - 1].id + 1;
@@ -42,7 +38,9 @@ auth.post("/auth/signup", (req, res) => {
   };
   users.push(newUser);
   fs.writeFileSync("./users.json", JSON.stringify({ users }));
+  console.log(process.env.JWT_SECRET_KEY);
   if (!process.env.JWT_SECRET_KEY) {
+    console.log("pp");
     res.statusCode = 500;
     return res.end();
   }
@@ -53,9 +51,18 @@ auth.post("/auth/signup", (req, res) => {
 });
 
 auth.get("/me", (req, res) => {
-  const token = req.cookies;
+  const { authorization } = req.headers;
+  if (!authorization) {
+    res.statusCode = 400;
+    return res.send("로그인되지 않았습니다.");
+  }
+  const token = authorization.replace("Bearer ", "");
+  console.log(token);
+  //console.log(req.headers);
   //const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+  const decoded_user = jwt.verify(token, process.env.JWT_SECRET_KEY!);
+  console.log(decoded_user);
 });
 export default auth;
 
-//! bcrypt 비밀번호 암호화 저장,  로그인 : 비밀번호를 복호화해서 비밀번호 맞는지 확인, token return, /me 완성하기
+//! bcrypt 비밀번호 암호화 저장,  로그인 : 비밀번호를 복호화해서 비밀번호 맞는지 확인, token return,
